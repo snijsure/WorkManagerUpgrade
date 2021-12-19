6b0f4c8d9781aea892e700e32c43d60d82dde333
Subodh Nijsure <sunijsur@microsoft.com>
2021-12-18 18:07:43 -0800

    Save periodic exec time in shared prefs

--- app/src/main/java/com/snijsure/workmanagerupgrade/MainActivity.kt
+++ app/src/main/java/com/snijsure/workmanagerupgrade/MainActivity.kt
@@ -1,43 +1,77 @@
 package com.snijsure.workmanagerupgrade
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
+import androidx.preference.PreferenceManager
 import androidx.work.*
 import com.snijsure.workmanagerupgrade.databinding.ActivityMainBinding
-import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val cal: Calendar = Calendar.getInstance()
-    private val timeFormat: SimpleDateFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault());
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater).apply {
             setContentView(root)
+            simpleChronometer.start()
         }
+        setupButton()
+    }
 
-        binding.startWorkButton.setOnClickListener {
-            binding.workManagerProgress.append("Work has been scheduled\n")
-            WorkManager.getInstance(application).enqueueUniquePeriodicWork(
-                WORKER_TAG,
-                ExistingPeriodicWorkPolicy.REPLACE,
-                getPeriodicWorkRequest()
-            )
-            binding.startWorkButton.setText(R.string.work_started)
-            binding.startWorkButton.isClickable = false
-            binding.startWorkButton.isEnabled = false
-
-        }
-        WorkManager.getInstance(application).getWorkInfosByTagLiveData(WORKER_TAG).observe(
-            this,
-            sampleWorkInfoObserver()
+    private fun workStatusUpdate(
+        workAlreadyScheduled: Boolean,
+        id: UUID,
+    ) {
+        binding.workManagerProgress.setText(
+            if (workAlreadyScheduled)
+                R.string.work_already_started
+            else
+                R.string.new_work_started
         )
+        binding.workManagerProgress.append("\n")
+        binding.startWorkButton.isClickable = false
+        binding.startWorkButton.isEnabled = false
+        WorkManager.getInstance(application).getWorkInfoByIdLiveData(id)
+            .observe(this, { workStatus ->
+                if (workStatus.state == WorkInfo.State.ENQUEUED) {
+                    val prefs: SharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(application)
+                    val workExecTime = prefs.getString(EXEC_TIME, "Unknown")
+                    if (!workExecTime.isNullOrBlank()) {
+                        binding.workManagerProgress.append("$workExecTime -> work completed\n")
+                    } else {
+                        binding.workManagerProgress.append("No exec time\n")
+                    }
+                }
+            })
+    }
+
+    private fun setupButton() {
+        val workInfoList: MutableList<WorkInfo>? =
+            WorkManager.getInstance(application).getWorkInfosByTag(WORKER_TAG).get()
+        if (!workInfoList.isNullOrEmpty()) {
+            workStatusUpdate(
+                workAlreadyScheduled = true,
+                id = workInfoList[0].id,
+            )
+        } else {
+            val workRequest = getPeriodicWorkRequest()
+            binding.startWorkButton.setOnClickListener {
+                WorkManager.getInstance(application).enqueueUniquePeriodicWork(
+                    WORKER_TAG,
+                    ExistingPeriodicWorkPolicy.REPLACE,
+                    workRequest
+                )
+                workStatusUpdate(
+                    workAlreadyScheduled = false,
+                    id = workRequest.id,
+                )
+            }
+        }
     }
 
     private fun getConstraints(): Constraints {
@@ -47,28 +81,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getPeriodicWorkRequest(): PeriodicWorkRequest {
-        return PeriodicWorkRequest.Builder(SampleWorker::class.java,
+        return PeriodicWorkRequest.Builder(
+            SampleWorker::class.java,
             INTERVAL_15_MIN,
             TimeUnit.MINUTES
         )
             .addTag(WORKER_TAG)
             .setConstraints(getConstraints())
             .build()
-    }
-
-    private fun sampleWorkInfoObserver(): Observer<List<WorkInfo>> {
-        return Observer { listOfWorkInfo ->
-            val currentTime: String = timeFormat.format(cal.time)
-            if (listOfWorkInfo.isNullOrEmpty()) {
-                binding.workManagerProgress.append("$currentTime -> No worker info\n")
-                return@Observer
-            }
-            val workInfo = listOfWorkInfo[0]
-            binding.workManagerProgress.append("$currentTime -> work status ${workInfo.state}\n")
-        }
-    }
-
-    companion object {
-        private val TAG = MainActivity::class.java.simpleName
     }
 }
--- app/src/main/java/com/snijsure/workmanagerupgrade/SampleWorker.kt
+++ app/src/main/java/com/snijsure/workmanagerupgrade/SampleWorker.kt
@@ -1,13 +1,22 @@
 package com.snijsure.workmanagerupgrade
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import androidx.preference.PreferenceManager
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.Calendar
 
-class SampleWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
+class SampleWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+    private val timeFormat: SimpleDateFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+    private val prefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
 
     override fun doWork(): Result {
+        val workExecTime: String = timeFormat.format(Calendar.getInstance().time)
+        prefs.edit().putString(EXEC_TIME, workExecTime).apply()
         Log.d(TAG, "SampleWorker start task ${Thread.currentThread().name}")
         return Result.success()
     }
